efd4c2d9e608f9f48ebc5c36005488510b713db0
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-05-17 17:20:52 +0200

    build: add CI deployment on gh-pages

---
+++ .travis/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+( yarn prettify:check || yarn lint || true ) && yarn test
+
+if [ "$TRAVIS_BRANCH" = "master" ];
+then
+    
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+    
+    yarn prod
+    
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/sonarplanet/build-ci-test.git gh-pages > /dev/null
+    
+    cd gh-pages
+    
+    git rm -r .
+    git add -A
+    git commit -m "[CI] Clear branch"
+    
+    mv ../dist/* .
+    
+    git add -f .
+    git commit -m "[CI]: publish master#$TRAVIS_COMMIT | Build $TRAVIS_BUILD_NUMBER | $(date '+%Y-%m-%d %H:%M:%S')"
+    
+    git push --quiet --set-upstream origin gh-pages > /dev/null
+    
+else
+    yarn prod
+fi
