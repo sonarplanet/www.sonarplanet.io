0de0492476b6ee46c4e0558d7953d8abbfc5fde8
Fabien TREGUER <fabien.treguer@spacelephant.org>
2018-05-30 17:00:56 +0200

    Fix: update for review

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,16 @@
 $(document).ready(() => {
   const DATA_TEXT = [
     'waiting confirmation of your transactions.',
-    'waiting execution of your transactions',
+    'waiting execution of your transactions.',
     'tracking changes on your balance of your public keys.',
-    'looking for your crypto'];
+    'looking for your crypto.'];
 
   let typeWriter = (text: string, i: number, fnCallback: Function) => {
     if (i < text.length) {
       document.getElementById('typewriter').innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
       setTimeout(function() {
         typeWriter(text, i + 1, fnCallback);
-      }, 100);
+      }, 50);
     } else {
       if (typeof fnCallback === 'function') {
         setTimeout(fnCallback, 700);
@@ -22,7 +22,7 @@ $(document).ready(() => {
     if (typeof DATA_TEXT[index] === 'undefined') {
       setTimeout(function() {
         startTextAnimation(0);
-      }, 2000);
+      }, 0);
     }
     if (DATA_TEXT[index] && index < DATA_TEXT[index].length) {
       typeWriter(DATA_TEXT[index], 0, function() {
